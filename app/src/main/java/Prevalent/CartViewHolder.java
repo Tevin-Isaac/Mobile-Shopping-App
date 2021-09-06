8678f4a57e6c9072448b778895a697fe29fa8f8f
Tevin-creator <isaactevin67@gmail..com>
2021-09-06 23:28:07 +0300

    added cartview holder in prevalent folder

--- app/src/main/java/Prevalent/CartViewHolder.java
+++ app/src/main/java/Prevalent/CartViewHolder.java
@@ -1,4 +1,35 @@
 package Prevalent;
 
-public class CartViewHolder {
+
+
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.brandykicks.Interface.ItemClickListener;
+import com.example.brandykicks.R;
+
+import Interface.ItemClickListener;
+
+public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView txtProductName,txtProductPrice,txtProductQuantity;
+    private ItemClickListener itemClickListener;
+
+    public CartViewHolder(View itemView) {
+        super(itemView);
+        txtProductName = itemView.findViewById(R.id.cart_product_name);
+        txtProductPrice = itemView.findViewById(R.id.cart_product_price);
+        txtProductQuantity = itemView.findViewById(R.id.cart_product_quantity);
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.onClick(view,getAdapterPosition(),false);
+    }
+
+    public void setItemClickListner(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
 }
