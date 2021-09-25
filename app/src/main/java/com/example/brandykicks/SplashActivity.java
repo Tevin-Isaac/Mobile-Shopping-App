70a05d94266ec69caa84b64e7d245459a7c3c184
Tevin-creator <isaactevin67@gmail..com>
2021-09-25 23:05:55 +0300

    added Loitte animation in splash screen in SplashActivity.java

--- app/src/main/java/com/example/brandykicks/SplashActivity.java
+++ app/src/main/java/com/example/brandykicks/SplashActivity.java
@@ -1,20 +1,25 @@
 package com.example.brandykicks;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
 public class SplashActivity extends AppCompatActivity {
 
+    LottieAnimationView lottie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        lottie=findViewById(R.id.lottie);
+//        lottie.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -29,6 +34,6 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
                 //the current activity will get finished.
             }
-        }, 4000);
+        }, 5000);
     }
 }
